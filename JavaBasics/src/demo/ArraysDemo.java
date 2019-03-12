ec9990d941356c15612cc7a4866c91e78790fb3a
“solai” <solai.muthukumar@gmail.com>
2019-03-11 21:38:53 -0400

    updated the ArraYSdEMO CLASS

--- JavaBasics/src/demo/ArraysDemo.java
+++ JavaBasics/src/demo/ArraysDemo.java
@@ -2,6 +2,11 @@ package demo;
 
 public class ArraysDemo {
 	
+	
+	public static void month() {
+		
+		System.out.println("Today is the month of March");
+	}
 	public static void main(String[] args) {
 		int[] numbers = new int[20];
 		
